77e0ac9e935df185670b0b276da37e412eaa8e50
mahimagupta24 <mahimagupta2401@gmail.com>
2022-12-21 17:29:09 +0530

    new component added

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import IsTriangle from "./isTriangle";
+
+function Header() {
+  return (
+    <div>
+      <a href="/">IsTriangle</a>
+      <a href="/hypoteneus">Hypoteneus</a>
+      <a href="/quiz">Quiz</a>
+    </div>
+  );
+}
+
+export default Header;
